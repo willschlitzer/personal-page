651bd70e4b6ac8372caa30c96ef1dbf05b11965b
Will Schlitzer <schlitzer90@gmail.com>
2020-05-09 17:58:01 +0900

    Fix typo

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ app.config["DEBUG"] = True
 @app.route("/", methods=["GET"])
 def home():
     return (
-        "<h1>Will Schlitzer</h1><p>An attempt at a personal page to learn Flask adn web app development</p>"
+        "<h1>Will Schlitzer</h1><p>An attempt at a personal page to learn Flask adn web app development.</p>"
     )
 
 app.run()
