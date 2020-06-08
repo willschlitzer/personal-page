9a777e60ff0b6d25fd7b1bf8962a0be608eca41e
Will Schlitzer <schlitzer90@gmail.com>
2020-06-08 19:29:48 +0900

    Add About page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return "<h1>Will Schlitzer's Page</h1><p>An attempt at a personal page to learn Flask and web app development.</p>"
 
+@app.route("/about", methods=["GET"])
+def about():
+    return "<h1>About Schlitz</h1><p>I'm making a website.</p>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
