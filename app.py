1708dcc4af781e7bfb9e97e6a7713eace95cabc3
Will Schlitzer <schlitzer90@gmail.com>
2021-05-09 13:17:24 +0100

    update layout files

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def main():
 
 @app.route("/", methods=["GET"])
 def home():
-    return "<h1>Will Schlitzer's Page</h1><p>This page is being built to learn Flask.</p>"
+    return flask.render_template("home/home.html")
 
 @app.route("/about", methods=["GET"])
 def about():
