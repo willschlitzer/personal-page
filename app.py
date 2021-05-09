6fea7ae4047933bc8a6603bfde8003e0f515a912
Will Schlitzer <schlitzer90@gmail.com>
2021-05-09 13:18:05 +0100

    run black

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 def main():
     app.run(debug=True)
+
 
 @app.route("/", methods=["GET"])
 def home():
     return flask.render_template("home/home.html")
+
 
 @app.route("/about", methods=["GET"])
 def about():
